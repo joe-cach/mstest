ee8855a4ac87e5e599d7b65ee1cf4f7a10aca007
joe <joseph.cacharani@gmail.com>
2022-09-03 20:44:41 +0200

    UnitTest1 SumValues

---
+++ ApplicationCore/Services/Sum.cs
@@ -0,0 +1,16 @@
+﻿namespace ApplicationCore.Services
+{
+    public class Sum
+    {
+        /// <summary>
+        /// SUM VALUES
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int SumValues(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UnitTest/Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// SUM VALUES TEST
+        /// </summary>
+        [TestMethod]
+        public void SumValues()
+        {
+            //AAA
+
+            //Arrange
+            ApplicationCore.Services.Sum sum = new ApplicationCore.Services.Sum();
+
+            //Act
+            var result = sum.SumValues(3,4);
+
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+    }
+}
